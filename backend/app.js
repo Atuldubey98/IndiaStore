472f830eff88acec3a55a4253fa722faf13dd72f
Atul Dubey <atuldubey017@gmail.com>
2022-02-18 22:38:34 +0530

    Added app

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,6 @@ const category = require("./api/routes/category");
 const orders = require("./api/routes/orders");
 const cors = require("cors");
 const app = express();
-
-//Added Comments
-// Com File
 app.use(cors());
 app.use(bodyParser.json());
 app.get("/api/v1", (req, res) => {
