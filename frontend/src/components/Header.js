fd1bfc069a66371322a839e3030d05c6d7516e14
Atul Dubey <atuldubey017@gmail.com>
2022-02-19 22:57:55 +0530

    Header added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
+import { useSelector } from "react-redux";
 const Header = () => {
+  const email = useSelector((state) => state.userAccess.user.email);
   return (
     <div className="header">
       <h2>India Store</h2>
       <div className="header_links">
         <Link to={"/"}>Home</Link>
+        <Link to={"/"}>Cart</Link>
+        <Link to={"/"}>Orders</Link>
+        <Link to={"/"}>Profile</Link>
+        <span>{email}</span>
       </div>
     </div>
   );
