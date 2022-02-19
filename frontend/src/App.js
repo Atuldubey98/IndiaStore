9a192435a1723a6544a03035da19f8f333719e53
Atul Dubey <atuldubey017@gmail.com>
2022-02-19 18:47:16 +0530

    useAuth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 import Login from "./pages/Login";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
 const App = () => {
   return (
     <div className="app">
-      <Login />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Homepage />} />
+      </Routes>
     </div>
   );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
