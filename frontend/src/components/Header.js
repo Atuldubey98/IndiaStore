8dd43d2543771918ec3ea558726e4673955e1106
Atul Dubey <atuldubey017@gmail.com>
2022-02-20 19:05:44 +0530

    Header commited

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,32 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
+import { ShoppingCart, Home, Face, ShoppingBasket } from "@material-ui/icons";
+import { Badge } from "@mui/material";
+import { useSelector } from "react-redux";
 const Header = () => {
+  const count = useSelector((state) => state.cartAccess.cart.length);
   return (
     <div className="header">
       <h2>India Store</h2>
-      <div className="header_links">
-        <Link to={"/"}>Home</Link>
-        <Link to={"/"}>Cart</Link>
-        <Link to={"/"}>Orders</Link>
-        <Link to={"/"}>Profile</Link>
+      <div className="header__links">
+        <div className="header__link">
+          <Home />
+          <Link to={"/"}>Home</Link>
+        </div>
+        <div className="header__link">
+          <Badge badgeContent={count}>
+            <ShoppingCart />
+          </Badge>
+          <Link to={"/"}>Cart</Link>
+        </div>
+        <div className="header__link">
+          <ShoppingBasket />
+          <Link to={"/"}>Orders</Link>
+        </div>
+        <div className="header__link">
+          <Face />
+          <Link to={"/"}>Profile</Link>
+        </div>
       </div>
     </div>
   );
