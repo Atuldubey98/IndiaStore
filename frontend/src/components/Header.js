c8a2998108cdda6d4eccc599b11a624045686964
Atul Dubey <atuldubey017@gmail.com>
2022-02-20 19:41:04 +0530

    Header edited

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,32 +1,59 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
-import { ShoppingCart, Home, Face, ShoppingBasket } from "@material-ui/icons";
+import {
+  ShoppingCart,
+  Home,
+  Face,
+  ShoppingBasket,
+  Search,
+} from "@material-ui/icons";
 import { Badge } from "@mui/material";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 const Header = () => {
   const count = useSelector((state) => state.cartAccess.cart.length);
+  const [search, setSearch] = useState("");
+  const onSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+  const [isSearch, setIsSearch] = useState(false);
+  const handleSearchVisibility = () => {
+    setIsSearch((s) => !s);
+  };
   return (
     <div className="header">
       <h2>India Store</h2>
-      <div className="header__links">
-        <div className="header__link">
-          <Home />
-          <Link to={"/"}>Home</Link>
-        </div>
-        <div className="header__link">
-          <Badge badgeContent={count}>
-            <ShoppingCart />
-          </Badge>
-          <Link to={"/"}>Cart</Link>
-        </div>
-        <div className="header__link">
-          <ShoppingBasket />
-          <Link to={"/"}>Orders</Link>
-        </div>
-        <div className="header__link">
-          <Face />
-          <Link to={"/"}>Profile</Link>
-        </div>
+      <div className="header__content">
+        <Search onClick={handleSearchVisibility} />
+        {isSearch ? (
+          <input
+            placeholder="Search..."
+            name="header__search"
+            value={search}
+            onChange={onSearchChange}
+          />
+        ) : (
+          <div className="header__links">
+            <div className="header__link">
+              <Home />
+              <Link to={"/"}>Home</Link>
+            </div>
+            <div className="header__link">
+              <Badge badgeContent={count}>
+                <ShoppingCart />
+              </Badge>
+              <Link to={"/"}>Cart</Link>
+            </div>
+            <div className="header__link">
+              <ShoppingBasket />
+              <Link to={"/"}>Orders</Link>
+            </div>
+            <div className="header__link">
+              <Face />
+              <Link to={"/"}>Profile</Link>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
