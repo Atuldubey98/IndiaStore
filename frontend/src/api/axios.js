20982cafcee6500b24bad1e873a2adc110b0ae45
Atul Dubey <atuldubey017@gmail.com>
2022-03-28 15:32:03 +0530

    Commit on master

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:9000/api/v1/",
+  baseURL: "http://indiastore-env.eba-e5uan2td.ap-south-1.elasticbeanstalk.com/api/v1/",
 });
 
 export default axiosInstance;
