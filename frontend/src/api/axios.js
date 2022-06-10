e09fbbfe22df8259c18ac24f3739c09c0f540b22
Atul Dubey <atuldubey017@gmail.com>
2022-06-11 00:17:54 +0530

    deploy

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://indiastore-app.herokuapp.com",
+  baseURL: "https://indiastore-app.herokuapp.com/api/v1/",
   withCredentials :  true
 });
 
