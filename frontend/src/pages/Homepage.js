1e5125290edebfc6d6c4a0acafdfcfedce2c9640
Atul Dubey <atuldubey017@gmail.com>
2022-06-11 16:29:02 +0530

    Orders

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -38,10 +38,10 @@ const Homepage = () => {
   const closeModal = () => {
     navigate("/");
   };
-  const onSearchFormSubmit = (e)=>{
+  const onSearchFormSubmit = (e) => {
     e.preventDefault();
     console.log(search);
-  }
+  };
   const customStyles = {
     content: {
       top: "50%",
@@ -57,7 +57,7 @@ const Homepage = () => {
     },
   };
   useEffect(() => {
-    const fetchProducts = async () => {
+    (async () => {
       try {
         dispatch(setProductLoading(true));
         const response = await axiosInstance.get("products/all");
@@ -67,79 +67,83 @@ const Homepage = () => {
       } catch (error) {
         setProductError(error);
       }
-    };
-    fetchProducts();
+    })();
   }, [dispatch]);
-  const [search, setSearch] = useState('');
-  const onSearchChange = (e)=>{
+  const [search, setSearch] = useState("");
+  const onSearchChange = (e) => {
     setSearch(e.target.value);
-  }
+  };
   return (
     <div className={"homepage"}>
       <Header />
-      <div className="homepage__filters">
-        <div className="homepage__filter">
-          <Button
-            onClick={() => onCategoryClick(true, query.get("categoryId"))}
-            variant={query.get("categoryId") ? "" : "contained"}
-          >
-            All
-          </Button>
-        </div>
-
-        {categories.map((c) => (
-          <div key={c.categoryId} className="homepage__filter">
+      <div className="homepage__container">
+        <div className="homepage__filters">
+          <h3 className="homepage__cathead">
+            Categories
+          </h3>
+          <div className="homepage__filter">
             <Button
-              variant={
-                query.get("categoryId") === c.categoryId ? "contained" : ""
-              }
-              onClick={() => onCategoryClick(false, c.categoryId)}
-              key={c.categoryId}
+              onClick={() => onCategoryClick(true, query.get("categoryId"))}
+              variant={query.get("categoryId") ? "outlined" : "contained"}
             >
-              {`${c.categoryName} (${
-                query.get("categoryId") === c.categoryId && products
-                  ? products.length
-                  : 0
-              })`}
+              All
             </Button>
           </div>
-        ))}
-      </div>
-      <div className={loading ? "homepage__load" : "homepage__products" }>
-        {loading ? (
-          <CircularProgress />
-        ) : products && products.length > 0 ? (
-          products.map((product) => (
-            <Product key={product.productId} product={product} />
-          ))
-        ) : (
-          <h1 className="no_products">{"No products found"}</h1>
-        )}
-        <Modal
-          isOpen={query.get("searchModal") ? true : false}
-          onRequestClose={closeModal}
-          style={customStyles}
-          contentLabel="Search"
-        >
-          <h1 style={{ margin: "auto" }}>
-            <i>India Store Search</i>
-          </h1>
-          <div className="homepage__search">
-            <form onSubmit={onSearchFormSubmit}>
-              <input
-                onChange={onSearchChange}
-                value={search}
-                type="text"
-                name="search"
-                placeholder="Enter search item..."
-              />
-              <IconButton type="submit" >
-                <Search fontSize="large" />
-              </IconButton>
-            </form>
-          </div>
-        </Modal>
-        {error && <h3>Some error occured</h3>}
+
+          {categories.map((c) => (
+            <div key={c.categoryId} className="homepage__filter">
+              <Button
+                variant={
+                  query.get("categoryId") === c.categoryId ? "contained" : "outlined"
+                }
+                onClick={() => onCategoryClick(false, c.categoryId)}
+                key={c.categoryId}
+              >
+                {`${c.categoryName} (${
+                  query.get("categoryId") === c.categoryId && products
+                    ? products.length
+                    : 0
+                })`}
+              </Button>
+            </div>
+          ))}
+        </div>
+        <div className={loading ? "homepage__load" : "homepage__products"}>
+          {loading ? (
+            <CircularProgress />
+          ) : products && products.length > 0 ? (
+            products.map((product) => (
+              <Product key={product.productId} product={product} />
+            ))
+          ) : (
+            <h1 className="no_products">{"No products found"}</h1>
+          )}
+          <Modal
+            isOpen={query.get("searchModal") ? true : false}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Search"
+          >
+            <h1 style={{ margin: "auto" }}>
+              <i>India Store Search</i>
+            </h1>
+            <div className="homepage__search">
+              <form onSubmit={onSearchFormSubmit}>
+                <input
+                  onChange={onSearchChange}
+                  value={search}
+                  type="text"
+                  name="search"
+                  placeholder="Enter search item..."
+                />
+                <IconButton type="submit">
+                  <Search fontSize="large" />
+                </IconButton>
+              </form>
+            </div>
+          </Modal>
+          {error && <h3>Some error occured</h3>}
+        </div>
       </div>
       {cart.length > 0 && <BuyProduct />}
     </div>
---
+++ frontend/src/redux/constants/orderActionTypes.js
@@ -0,0 +1,3 @@
+export const ORDER_ERROR  = "ORDER_ERROR";
+export const ORDER_LOADING = "ORDER_LOADING";
+export const ORDER_SET="ORDER_SET";
